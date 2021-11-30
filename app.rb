8948d656026c25001ec002efa8e6a0feab59376e
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-30 15:27:26 +0000

    Add route in Battle for GET /

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
-class Battle < Sinatra::base
+class Battle < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Hello Battle!'
   end
 
   run! if app_file ==$0
