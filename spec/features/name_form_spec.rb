a1e5b28a5e4203e0606c37030704498402ddeaae
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-11-30 16:08:08 +0000

    Update test to check for twoo players

--- spec/features/name_form_spec.rb
+++ spec/features/name_form_spec.rb
@@ -1,8 +1,9 @@
 feature 'player name form' do
   scenario 'Can enter names and see them onscreen' do
     visit('/')
-    fill_in('name', with: 'Player')
+    fill_in('name_1', with: 'Player 1')
+    fill_in('name_2', with: 'Player 2')
     click_button 'submit'
-    expect(page).to have_content ('Player')
+    expect(page).to have_content ('Player 1 vs Player 2')
   end
 end
