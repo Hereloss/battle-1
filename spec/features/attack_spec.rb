ac41b6f5fed12f202836614576a95868dd19d4d0
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-02 13:36:05 +0000

    Rename attack test

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,5 +1,5 @@
 feature 'attack' do
-  scenario 'Attacking reduces hitpoints' do
+  scenario 'Player 1 attacks player 2 and gets a notification' do
     sign_in_and_play
     click_button 'Attack!'
     expect(page).to have_content('Player 2 was attacked!')
