1319d98bfdf933efbc0b38271a4fe0ca975495ca
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-02 13:35:30 +0000

    Implement notification on attacking player 2

--- app.rb
+++ app.rb
@@ -14,13 +14,20 @@ class Battle < Sinatra::Base
 
   get '/play' do
     @names = session[:names]
+    @player_1_hp = 100
+    @player_2_hp = 100
     erb :play
+  end
+
+  get '/attack' do
+    'Player 2 was attacked!'
   end
 
   post '/names' do
     session[:names] = params
     redirect '/play'
   end
+
 
   run! if app_file == $0
 end
--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,7 @@
 feature 'attack' do
   scenario 'Attacking reduces hitpoints' do
     sign_in_and_play
-    find('#player-1-attack').click
-    expect(page).to have_content('Player 2 - 90 HP')
+    click_button 'Attack!'
+    expect(page).to have_content('Player 2 was attacked!')
   end
 end
